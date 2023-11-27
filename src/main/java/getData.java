4a184decacc739db9f0dbad21d2a35c2e4ff301c
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-11-27 15:45:01 -0800

    Create a Java Bean class to get and set the employee data

---
+++ src/main/java/getData.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author javier
+ */
+@Named(value = "getData")
+@Dependent
+public class getData {
+
+    /**
+     * Creates a new instance of getData
+     */
+    public getData() {
+    }
+    
+}
