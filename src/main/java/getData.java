a948f0f0709019cf121e9434bb6598b8901b9db9
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-11-28 08:55:04 -0800

    Add method to check human validation form input

--- src/main/java/getData.java
+++ src/main/java/getData.java
@@ -1,6 +1,8 @@
 
+import java.util.Random;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -16,16 +18,23 @@ import javax.faces.bean.RequestScoped;
  */
 //@Named(value = "getData")
 //@Dependent
-@ManagedBean(name="obj",eager=true)
-@RequestScoped
+@ManagedBean
+@SessionScoped
 public class getData {
+    private Random rand;
+    private int upperbound;
     private String ename;
     private String address;
-    private int age;
-    /**
-     * Creates a new instance of getData
-     */
+    private int seniority;
+    private String department;
+    private int numA;
+    private int numB;
+    private int numC;
+    
     public getData() {
+        rand = new Random();
+        upperbound = 50;
+        numC = rand.nextInt(upperbound);
     }
 
     public String getEname() {
@@ -44,13 +53,56 @@ public class getData {
         this.address = address;
     }
 
-    public int getAge() {
-        return age;
+    public int getSeniority() {
+        return seniority;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setSeniority(int seniority) {
+        this.seniority = seniority;
     }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getNumA() {
+        return numA;
+    }
+
+    public void setNumA(int numA) {
+        this.numA = numA;
+    }
+
+    public int getNumB() {
+        return numB;
+    }
+
+    public void setNumB(int numB) {
+        this.numB = numB;
+    }
+
+    public int getNumC() {
+        return numC;
+    }
+
+    public void setNumC(int numC) {
+        this.numC = numC;
+    }
+
+    /**
+     * Creates a new instance of getData
+     */
     
+    public String checkAnswer () {
+        if (numA + numB == numC) {
+             return "Passed";
+        } else {
+            return "Failed, please try again";
+        }
+    }
     
 }
